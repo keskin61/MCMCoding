8fc019457a3427219ff040036836305400d596cf
MCMCoding <mhs_57@hotmail.de>
2014-06-02 11:33:17 +0200

    Version 0.6.9 Meilenstein4 vorname+name+email+matrikelnr+handynr Valdierung ergänzt.

--- indexscript.js
+++ indexscript.js
@@ -1,6 +1,7 @@
 function validateInput () {
 
-  if (document.myform.vorname.value == "") {
+    //Validierung: Vorname.
+    if (document.myform.vorname.value == "") {
     console.log("vorname leere Eingabe.");
     alert("Einige Eingaben sind fehlerhaft.Bitte überprüfen Sie ihre Eingaben");
     document.myform.vorname.focus();
@@ -22,8 +23,8 @@ function validateInput () {
         }
         
     }
-
-  if (document.myform.name.value == "") {
+    //Validierung: Name.
+    if (document.myform.name.value == "") {
     console.log("name leere Eingabe.");
     alert("Einige Eingaben sind fehlerhaft.Bitte überprüfen Sie ihre Eingaben");
     document.myform.name.focus();
@@ -34,7 +35,6 @@ function validateInput () {
             if (document.myform.name.value.toLowerCase().charAt(i) < "a" ||
             document.myform.name.value.toLowerCase().charAt(i)  > "z"){
             flag=true;
-            
             }
         }
         if(flag){
@@ -45,33 +45,123 @@ function validateInput () {
         }
         
     }
-  /*
-  
-  
-  if (document.Formular.email.value == "") {
-    alert("Einige Eingaben sind fehlerhaft.Bitte überprüfen Sie ihre Eingaben");
-    document.Formular.Mail.focus();
-    return false;
-  }
-  if (document.Formular.email.value.indexOf("@") == -1) {
-    alert("Einige Eingaben sind fehlerhaft.Bitte überprüfen Sie ihre Eingaben");
-    document.Formular.Mail.focus();
-    return false;
-  }
-  if (document.Formular.matrikelnr.value == "") {
-    alert("Einige Eingaben sind fehlerhaft.Bitte überprüfen Sie ihre Eingaben");
-    document.Formular.Alter.focus();
-    return false;
-  }
-  var chkZ = 1;
-  for (i = 0; i < document.Formular.Alter.value.length; ++i)
-    if (document.Formular.Alter.value.charAt(i) < "0" ||
-        document.Formular.Alter.value.charAt(i) > "9")
-      chkZ = -1;
-  if (chkZ == -1) {
-    alert("Altersangabe keine Zahl!");
-    document.Formular.Alter.focus();
-    return false;
-  }
-  */
+    
+    //Validierung: email.
+    if (document.myform.email.value == "") {
+        console.log("email leere Eingabe.");
+        alert("Einige Eingaben sind fehlerhaft.Bitte überprüfen Sie ihre Eingaben");
+        document.myform.email.focus();
+        return false;
+    }
+    
+    //@-index finden. + überprüfen auf Einmalvorkommen.
+    var indxAt=-1;
+    var counter=0;
+    for(i=0; i < document.myform.email.value.length;++i){
+        if(document.myform.email.value.charAt(i)=='@'){
+            ++counter;
+            indxAt=i;
+        }
+    }
+    if(indxAt==-1 || (counter != 1)){
+        console.log("email ungültiges Format.-kein oder mehr als ein @-Zeichen");
+        alert("Einige Eingaben sind fehlerhaft.Bitte überprüfen Sie ihre Eingaben");
+        document.myform.email.focus();
+        return false;
+    }
+    // an dieser Stelle wurde die Position des @ zeichens gefunden.
+    //den punkt nach dem @ finden.
+    var indxPt=-1;
+    for(i=indxAt; i < document.myform.email.value.length;++i){
+        if(document.myform.email.value.charAt(i)=='.'){
+            indxPt=i;
+            //hier wird nur der Letzte Punkt gespeichert. bsp: @stud.hs.de
+        }
+    }   
+    if(indxPt==-1){
+        console.log("email ungültiges Format.-kein Punkt nach dem @-Zeichen");
+        alert("Einige Eingaben sind fehlerhaft.Bitte überprüfen Sie ihre Eingaben");
+        document.myform.email.focus();
+        return false;
+    }
+    
+    
+    var vorstring=document.myform.email.value.substring(0, indxAt);
+    var nachstring=document.myform.email.value.substring(indxAt+1, indxPt);
+    var domainstring=document.myform.email.value.substring(indxPt+1, document.myform.email.value.length);
+    console.log("vorstring:"+vorstring+","+"nachstring:"+nachstring+","+"domainstring:"+domainstring+".");
+    
+    var flagMail=false;
+    //substrings auf ungültige zeichen überprüfen.
+    if(domainstring=='de'||domainstring=='org'||domainstring=='net'||domainstring=='com'){
+        ;
+    }else{
+        console.log("email ungültiges Format.-gültige domainnames: de/org/net/com");
+        flagMail=true;
+    }
+    
+    if(flagMail){
+        console.log("email ungültiges Format.");
+        alert("Einige Eingaben sind fehlerhaft.Bitte überprüfen Sie ihre Eingaben");
+        document.myform.email.focus();
+        return false;
+    }
+
+    //Validierung: Matrikelnr.
+    if (document.myform.matrikelnr.value == "") {
+        console.log("matrikelnr leere Eingabe.");
+        alert("Einige Eingaben sind fehlerhaft.Bitte überprüfen Sie ihre Eingaben");
+        document.myform.matrikelnr.focus();
+        return false;
+    }else{
+        var flagMrtklnr = false;
+        
+        for (i = 0; i < document.myform.matrikelnr.value.length; ++i){
+            if (document.myform.matrikelnr.value.charAt(i) < "0" ||
+             document.myform.matrikelnr.value.charAt(i) > "9"){
+                console.log("matrikelnr enthält ungültige Zeichen.");
+                flagMrtklnr = true;
+            }
+             if(document.myform.matrikelnr.value.charAt(0)==0){
+                 console.log("matrikelnr beginnt mit 0. Ungültige Eingabe.");
+                 flagMrtklnr = true;
+             }
+        }
+        if (flagMrtklnr) {
+            alert("Einige Eingaben sind fehlerhaft.Bitte überprüfen Sie ihre Eingaben");
+            document.myform.matrikelnr.focus();
+            return false;
+        }
+    }
+
+    //Validierung: Handynummer.
+    if (document.myform.handy.value == "") {
+        console.log("handy leere Eingabe.");
+        alert("Einige Eingaben sind fehlerhaft.Bitte überprüfen Sie ihre Eingaben");
+        document.myform.handy.focus();
+        return false;
+    }else{
+        var flaghandy = false;
+        
+        for (i = 0; i < document.myform.handy.value.length; ++i){
+            if (document.myform.handy.value.charAt(i) < "0" ||
+             document.myform.handy.value.charAt(i) > "9"){
+                console.log("handynr enthält ungültige Zeichen.");
+                flaghandy = true;
+            }
+             if(document.myform.handy.value.charAt(0)!=0){
+                 console.log("handynr beginnt nicht mit 0. Ungültige Eingabe.");
+                 flaghandy = true;
+             }
+        }
+        if (flaghandy) {
+            alert("Einige Eingaben sind fehlerhaft.Bitte überprüfen Sie ihre Eingaben");
+            document.myform.handy.focus();
+            return false;
+        }
+    }
+      
+
+    
+
 }

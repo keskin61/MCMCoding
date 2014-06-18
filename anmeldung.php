9de2aa3dab885e7d2d7c63b56f495a82dd567568
seker57 <1210402@stud.hs-mannheim.de>
2014-06-18 17:58:09 +0200

    Version 0.7.4 Meilenstein5 Aufgabe1 Verbesserung. Filewrite.

--- anmeldung.php
+++ anmeldung.php
@@ -4,11 +4,23 @@
     </head>
     <body>
         <?php
+            
+            if($_GET['messe']=='CeBIT'){
+                $my_file='cebit.txt';
+            }else if($_GET['messe']=='ConIT'){
+                $my_file='conhit.txt';
+            }else if($_GET['messe']=='WebTech Con'){
+                $my_file='webtech.txt';
+            }
+            
             $output=$_GET['vorname']." ".$_GET['name'].", ".$_GET['matrikelnr'].", ".$_GET['email'].", ".$_GET['handy'].", ".$_GET['studiengang'].";\n";
-           $my_file = 'cebit.txt';
+           
             $handle = fopen($my_file, 'a') or die('Cannot open file:  '.$my_file);
             fwrite($handle, $output);
             fclose($handle);
+              
+             
+             
         ?>
     </body>
 </html>
